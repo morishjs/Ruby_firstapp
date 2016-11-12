e501e0b715c757af8db7a226fb73a2b7fe852705
Junsuk Park <park64kr63@gmail.com>
2016-11-12 17:50:30 +0900

    Fix create, add flash and error messages, create show

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,18 +1,27 @@
 class ArticlesController < ApplicationController
   def new
     @article = Article.new
-
   end
 
   def create
-    #render plain: params[:article].inspect
+
     @article = Article.new(article_params)
-    @article.save
-    redirect_to articles_show(@article)
+    if @article.save
+      flash[:notice] = "Article was successfully saved"
+      redirect_to article_path(@article)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @article = Article.find(params[:id])
   end
 
   private
     def article_params
+      #article 클래스의 (article.rb)
+      #"article"=>{"title"=>"aa", "description"=>"aasdf"} 이런 식으로 파라미터가 넘어옴
       params.require(:article).permit(:title, :description)
     end
 end
