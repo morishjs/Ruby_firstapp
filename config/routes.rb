b92ef259570649fffc28454a1e509947dd60e3ce
Junsuk Park <park64kr63@gmail.com>
2016-11-12 14:52:36 +0900

    new, crate action

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   get 'about', to: 'welcome#about'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
-
+  resources :articles
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
